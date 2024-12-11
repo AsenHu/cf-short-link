9e20abfd8bf5ff1987dbcf04b1981c2b4be90980
alongw <alongw@alongw.cn>
2024-12-11 19:23:02 +0800

    fix: request respones catch

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { useTokenStore } from '@/stores/token'
+import { message } from 'ant-design-vue'
 
 const base_url = import.meta.env.VITE_BASE_URL
 
@@ -13,6 +14,19 @@ const axiosInstance = axios.create({
     Authorization: `Bearer ${tokenStore.token}`,
   },
 })
+
+axiosInstance.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    console.log(error)
+    if (error.response?.data.ok == false) {
+      message.error(error.response.data.msg)
+    }
+    return Promise.resolve(error)
+  },
+)
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Response<T = any> = {
