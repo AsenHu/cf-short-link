5dc1dd727980bad92f2c8cc4e8cade566ad54e61
alongw <neko@awwa.cc>
2025-02-09 20:43:21 +0800

    feat: add filter

--- src/hooks/useManage.ts
+++ src/hooks/useManage.ts
@@ -7,54 +7,66 @@ import { getLink } from '@/apis/index'
 import type { shortLinkInstance } from '@/types/index'
 
 export const useManage = () => {
-  const currentPage = ref(1)
-  const pageSize = ref(10)
-  const total = ref(0)
+    const currentPage = ref(1)
+    const pageSize = ref(10)
+    const total = ref(0)
+    const searchTerm = ref('')
+    const oldSearchTerm = ref('')
 
-  const originalDataList = ref<shortLinkInstance[]>([])
+    const originalDataList = ref<shortLinkInstance[]>([])
 
-  const linkList = computed(() => {
-    const start = (currentPage.value - 1) * pageSize.value
-    const end = currentPage.value * pageSize.value
-    return originalDataList.value.slice(start, end)
-  })
-
-  const columns = [
-    {
-      title: 'Key',
-      dataIndex: 'key',
-      key: 'full',
-      ellipsis: true,
-    },
-    {
-      title: 'Action',
-      key: 'action',
-    },
-  ]
-
-  const getLinkList = async () => {
-    const { data: result } = await getLink()
-
-    if (result.data.list_complete !== true) {
-      message.error('Unable to get list')
-      return
-    }
-    originalDataList.value = result.data.links.map(e => {
-      return {
-        ...e.short,
-        url: e.url,
-        expiration: e.expiration,
-      }
+    const linkList = computed(() => {
+        if (oldSearchTerm.value !== searchTerm.value) {
+            currentPage.value = 1
+            oldSearchTerm.value = searchTerm.value
+        }
+        const filtered = originalDataList.value.filter(
+            (item) =>
+                item.key.includes(searchTerm.value) ||
+                item.url?.includes(searchTerm.value)
+        )
+        const start = (currentPage.value - 1) * pageSize.value
+        const end = currentPage.value * pageSize.value
+        return filtered.slice(start, end)
     })
-    total.value = originalDataList.value.length
-  }
 
-  return {
-    linkList,
-    columns,
-    getLinkList,
-    currentPage,
-    pageSize,
-    total,
-  }
+    const columns = [
+        {
+            title: 'Key',
+            dataIndex: 'key',
+            key: 'full',
+            ellipsis: true
+        },
+        {
+            title: 'Action',
+            key: 'action'
+        }
+    ]
+
+    const getLinkList = async () => {
+        const { data: result } = await getLink()
+
+        if (result.data.list_complete !== true) {
+            message.error('Unable to get list')
+            return
+        }
+        originalDataList.value = result.data.links.map((e) => {
+            return {
+                ...e.short,
+                url: e.url,
+                expiration: e.expiration
+            }
+        })
+        total.value = originalDataList.value.length
+    }
+
+    return {
+        linkList,
+        columns,
+        getLinkList,
+        currentPage,
+        pageSize,
+        total,
+        searchTerm
+    }
 }
