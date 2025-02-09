2bf8f91c47e19673fa697cb4adc58d2d518d4745
alongw <neko@awwa.cc>
2025-02-09 20:23:40 +0800

    feat: add search ipt

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { useNavStore } from '@/stores/nav'
 import { isPhone } from '@/utils/screen'
 
 defineOptions({
-  name: 'AppComponent',
+    name: 'AppComponent'
 })
 
 const selectedKeys = ref<string[]>()
@@ -15,140 +15,140 @@ const collapsed = ref<boolean>(isPhone())
 const navStore = useNavStore()
 
 onMounted(() => {
-  watch(
-    () => router.currentRoute.value.name,
-    name => {
-      selectedKeys.value = [name?.toString() || '']
-    },
-    { immediate: true },
-  )
+    watch(
+        () => router.currentRoute.value.name,
+        (name) => {
+            selectedKeys.value = [name?.toString() || '']
+        },
+        { immediate: true }
+    )
 })
 </script>
 
 <template>
-  <a-layout class="layout">
-    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
-      <div class="logo">
-        <router-link to="/">
-          <h1 v-show="!collapsed">CF Short Link</h1>
-          <h1 v-show="collapsed">CSL</h1>
-        </router-link>
-      </div>
-      <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
-        <a-menu-item
-          v-for="item in navStore.navList"
-          :key="item.key"
-          @click="
-            router.push({
-              name: item.key,
-            })
-          "
-        >
-          <component :is="item.icon" />
-          <span>{{ item.label }}</span>
-        </a-menu-item>
-      </a-menu>
-    </a-layout-sider>
-    <a-layout>
-      <a-layout-header style="background: #fff; padding: 0; padding-left: 25px">
-        <menu-unfold-outlined
-          v-if="collapsed"
-          class="trigger"
-          @click="() => (collapsed = !collapsed)"
-        />
-        <menu-fold-outlined
-          v-else
-          class="trigger"
-          @click="() => (collapsed = !collapsed)"
-        />
-      </a-layout-header>
-      <a-layout-content
-        :style="{
-          margin: '24px 16px',
-          padding: '24px',
-          background: '#fff',
-          minHeight: '280px',
-          height: '100%',
-        }"
-      >
-        <div class="page-content">
-          <router-view />
-        </div>
-      </a-layout-content>
+    <a-layout class="layout">
+        <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
+            <div class="logo">
+                <router-link to="/">
+                    <h1 v-show="!collapsed">CF Short Link</h1>
+                    <h1 v-show="collapsed">CSL</h1>
+                </router-link>
+            </div>
+            <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="inline">
+                <a-menu-item
+                    v-for="item in navStore.navList"
+                    :key="item.key"
+                    @click="
+                        router.push({
+                            name: item.key
+                        })
+                    "
+                >
+                    <component :is="item.icon" />
+                    <span>{{ item.label }}</span>
+                </a-menu-item>
+            </a-menu>
+        </a-layout-sider>
+        <a-layout>
+            <a-layout-header style="background: #fff; padding: 0; padding-left: 25px">
+                <menu-unfold-outlined
+                    v-if="collapsed"
+                    class="trigger"
+                    @click="() => (collapsed = !collapsed)"
+                />
+                <menu-fold-outlined
+                    v-else
+                    class="trigger"
+                    @click="() => (collapsed = !collapsed)"
+                />
+            </a-layout-header>
+            <a-layout-content
+                :style="{
+                    margin: '24px 16px',
+                    padding: '24px',
+                    background: '#fff',
+                    minHeight: '280px',
+                    height: '100%'
+                }"
+            >
+                <div class="page-content">
+                    <router-view />
+                </div>
+            </a-layout-content>
+        </a-layout>
     </a-layout>
-  </a-layout>
 </template>
 
 <style scoped lang="less">
 .layout {
-  height: 100vh;
-  overflow: hidden;
+    height: 100vh;
+    overflow: hidden;
 
-  .logo {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 50px;
-    color: #fff;
-    font-size: 18px;
-
-    h1 {
-      vertical-align: middle;
-      margin: 0;
-    }
-
-    a {
-      color: #fff;
-
-      &:hover {
+    .logo {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 50px;
         color: #fff;
-      }
+        font-size: 18px;
+
+        h1 {
+            vertical-align: middle;
+            margin: 0;
+        }
+
+        a {
+            color: #fff;
+
+            &:hover {
+                color: #fff;
+            }
+        }
     }
-  }
 }
 
 #components-layout-demo-custom-trigger .trigger {
-  font-size: 18px;
-  line-height: 64px;
-  padding: 0 24px;
-  cursor: pointer;
-  transition: color 0.3s;
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
 }
 
 #components-layout-demo-custom-trigger .trigger:hover {
-  color: #1890ff;
+    color: #1890ff;
 }
 
 #components-layout-demo-custom-trigger .logo {
-  height: 32px;
-  background: rgba(255, 255, 255, 0.3);
-  margin: 16px;
+    height: 32px;
+    background: rgba(255, 255, 255, 0.3);
+    margin: 16px;
 }
 
 .site-layout .site-layout-background {
-  background: #fff;
+    background: #fff;
 }
 
 .page-content {
-  overflow-y: auto;
-  height: 100%;
+    overflow-y: auto;
+    height: 100%;
 }
 
 ::-webkit-scrollbar {
-  height: 5px;
-  width: 5px;
-  padding: 0 5px;
+    height: 5px;
+    width: 5px;
+    padding: 0 5px;
 }
 
 ::-webkit-scrollbar-thumb {
-  background: #aaaa !important;
-  box-shadow: none !important;
-  border: none !important;
-  border-radius: 10px;
-  transition: background 0.3s ease;
+    background: #aaaa !important;
+    box-shadow: none !important;
+    border: none !important;
+    border-radius: 10px;
+    transition: background 0.3s ease;
 }
 
 ::-webkit-scrollbar-thumb:hover {
-  background: #aaa !important;
+    background: #aaa !important;
 }
 </style>
--- src/pages/Manage/Manage.vue
+++ src/pages/Manage/Manage.vue
@@ -4,67 +4,72 @@ import { useCreateLinkDialog } from '@/components/fc/index'
 import { useManage } from '@/hooks/useManage'
 
 defineOptions({
-  name: 'ManagePage',
+    name: 'ManagePage'
 })
 
-const { getLinkList, linkList, columns, currentPage, pageSize, total } =
-  useManage()
+const { getLinkList, linkList, columns, currentPage, pageSize, total } = useManage()
 
 const handleCreateLink = async () => {
-  await useCreateLinkDialog()
-  getLinkList()
+    await useCreateLinkDialog()
+    getLinkList()
 }
 
 const handleChangePage = (page: number, pageSizeValue: number) => {
-  currentPage.value = page
-  pageSize.value = pageSizeValue
+    currentPage.value = page
+    pageSize.value = pageSizeValue
 }
 
 onMounted(() => {
-  getLinkList()
+    getLinkList()
 })
 </script>
 
 <template>
-  <div class="manage">
-    <div class="create">
-      <a-button type="primary" @click="handleCreateLink">
-        Create a link
-      </a-button>
-    </div>
+    <div class="manage">
+        <div class="create">
+            <a-button type="primary" @click="handleCreateLink"> Create a link </a-button>
+        </div>
 
-    <div class="table">
-      <a-table
-        :dataSource="linkList"
-        :columns="columns"
-        :pagination="{
-          current: currentPage,
-          pageSize: pageSize,
-          total: total,
-          showSizeChanger: true,
-          onChange: handleChangePage,
-        }"
-      >
-        <!-- eslint-disable-next-line vue/no-unused-vars -->
-        <template #bodyCell="{ column, record }">
-          <template v-if="column.key === 'action'">
-            <a-button type="link">Details</a-button>
-          </template>
-        </template>
-      </a-table>
+        <div class="search">
+            <a-input placeholder="Filter" />
+        </div>
+
+        <div class="table">
+            <a-table
+                :dataSource="linkList"
+                :columns="columns"
+                :pagination="{
+                    current: currentPage,
+                    pageSize: pageSize,
+                    total: total,
+                    showSizeChanger: true,
+                    onChange: handleChangePage
+                }"
+            >
+                <!-- eslint-disable-next-line vue/no-unused-vars -->
+                <template #bodyCell="{ column, record }">
+                    <template v-if="column.key === 'action'">
+                        <a-button type="link">Details</a-button>
+                    </template>
+                </template>
+            </a-table>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped lang="less">
 .manage {
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 
-  .table {
-    margin-top: 20px;
-    max-width: 100%;
-    height: 300px;
-  }
+    .search {
+        margin-top: 20px;
+    }
+
+    .table {
+        margin-top: 20px;
+        max-width: 100%;
+        height: 300px;
+    }
 }
 </style>
